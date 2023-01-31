6267b0029c72602137e5f9b61b6c33fe780500ab
gukkame <guka245@gmail.com>
2023-01-31 22:41:31 +0200

    renamed files

--- fact-of-the-day/src/index.js
+++ fact-of-the-day/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/App.css';
+import App from './app';
+import './styles/app.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
